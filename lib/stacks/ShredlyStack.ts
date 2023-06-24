c478fcd43d1d429a2201ae8de925a31b46c8d90a
Shawn Torsitano <shawn.torsitano@gmail.com>
2023-06-24 19:05:42 -0400

    testing updates

--- lib/stacks/ShredlyStack.ts
+++ lib/stacks/ShredlyStack.ts
@@ -1,13 +1,14 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager'
-import { Distribution, OriginAccessIdentity, PriceClass, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront'
+import { CfnDistribution, CfnOriginAccessControl, Distribution, PriceClass, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront'
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins'
 import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53'
 import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets'
-import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3'
+import { BlockPublicAccess, Bucket, CfnBucketPolicy } from 'aws-cdk-lib/aws-s3'
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
 import { Construct } from 'constructs'
 import { BuildConfig } from '../buildUtils'
+import { Effect, PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
 
 
 export class ShredlyStack extends Stack {
@@ -27,9 +28,6 @@ export class ShredlyStack extends Stack {
             ]
         } )
 
-        const originAccessIdentity = new OriginAccessIdentity( this, 'originAccessIdentity' )
-        landingPageBucket.grantRead( originAccessIdentity )
-
         const hostedZone = HostedZone.fromLookup( this, 'shredlyHostedZone', {
             domainName: 'shred-ly.com.'
         } )
@@ -42,12 +40,19 @@ export class ShredlyStack extends Stack {
             validation: CertificateValidation.fromDns( hostedZone )
         } )
 
+        const originAccessControl = new CfnOriginAccessControl( this, 'OriginAccessControl', {
+            originAccessControlConfig: {
+                name: 'shredly-origin',
+                signingBehavior: 'always',
+                signingProtocol: 'sigv4',
+                originAccessControlOriginType: 's3'
+            }
+        } )
+
         const landingPageDistribution = new Distribution( this, 'landingPageDistribution', {
             defaultRootObject: 'index.html',
             defaultBehavior: {
-                origin: new S3Origin( landingPageBucket, {
-                    originAccessIdentity
-                } ),
+                origin: new S3Origin( landingPageBucket ),
                 viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS
             },
             domainNames: [
@@ -58,6 +63,25 @@ export class ShredlyStack extends Stack {
             enableLogging: true,
             priceClass: PriceClass.PRICE_CLASS_100
         } )
+
+        landingPageBucket.addToResourcePolicy( new PolicyStatement( {
+            effect: Effect.ALLOW,
+            actions: [ 's3:GetObject' ],
+            resources: [ landingPageBucket.arnForObjects( '*' ) ],
+            principals: [ new ServicePrincipal( 'cloudfront.amazonaws.com' ) ],
+            conditions: {
+                StringEquals: {
+                    'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${landingPageDistribution.distributionId}`
+                }
+            }
+        } ) )
+
+        const cfnBucketPolicy = landingPageBucket.policy?.node.defaultChild as CfnBucketPolicy
+        cfnBucketPolicy.addPropertyDeletionOverride( 'PolicyDocument.Statement.0' )
+
+        const cfnDistribution = landingPageDistribution.node.defaultChild as CfnDistribution
+        cfnDistribution.addPropertyOverride( 'DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity', '' )
+        cfnDistribution.addPropertyOverride( 'DistributionConfig.Origins.0.OriginAccessControlId', originAccessControl.ref )
 
 
         new ARecord( this, 'shredlyLandingPage', {
